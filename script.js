f9d225978d21b54c1922dc38e7723b3526f3b17a
alen <alen.a@qburst.com>
2022-10-06 11:53:47 +0530

    UI-final commit

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ fetch("./data.json")
             .then(skills =>
                 (emplData(data, skills)))
     });
-
-
+ 
+ 
 function emplData(data, skillSet) {
     let thead = document.getElementById("thead");
-    let table_row_head = document.createElement("tr");
-    table_row_head.setAttribute("id", "tr_head");
-    table_row_head.innerHTML = `<th>Employee ID</th><th>Employee Name</th><th>Designation</th><th>Email-ID</th><th>Skills</th><th>Actions</th>`
-    thead.appendChild(table_row_head);
+    let tableRowHead = document.createElement("tr");
+    tableRowHead.setAttribute("id", "tr_head");
+    tableRowHead.innerHTML = `<th>Employee ID</th><th>Employee Name</th><th>Designation</th><th>Email-ID</th><th>Skills</th><th>Actions</th>`
+    thead.appendChild(tableRowHead);
     data.forEach(item => {
         let tbody = document.getElementById("tbody");
-        let table_row_data = document.createElement("tr");
-        table_row_data.setAttribute("class", "tr_data");
+        let tableRowData = document.createElement("tr");
+        tableRowData.setAttribute("class", "tr_data");
         let reqSkill = item.skills.map(skillId => {
             for (let skillObj of skillSet) {
                 if (skillObj.skill_id == skillId) {
@@ -26,96 +26,107 @@ function emplData(data, skillSet) {
                 }
             }
         })
-        table_row_data.innerHTML = `<td>${item.emp_id}</td><td>${item.emp_name}</td>
-        <td>${item.emp_desig}</td><td>${item.email_id}</td><td>${reqSkill.join(", ")}</td>
-        <td><button id="dlt-btn" onclick="dltModal()">Delete</button></td>`
-        tbody.appendChild(table_row_data);
+        tableRowData.innerHTML = `<td>${item.emp_id}</td><td>${item.emp_name}</td>
+       <td>${item.emp_desig}</td><td>${item.email_id}</td><td>${reqSkill.join(", ")}</td>
+       <td><img src="./images/edit-icon.png" id="edit-btn" onclick="edtModal()">
+       <img src="./images/delete-icon.png" id="dlt-btn" onclick="dltModal()"></td>`
+        tbody.appendChild(tableRowData);
     });
-
 }
 //Add-Modal Box
 let modal = document.getElementById("modal-box");
-
+let editModal = document.getElementById("edit-modal-content")
 // Get the button that opens the modal
 let btn = document.getElementById("add-btn");
-
+ 
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
+let span = document.getElementsByTagName("span")[0];
+ 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
     modal.style.display = "block";
+ 
 }
-
+ 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
+    editModal.style.display = "none";
 }
-
+ 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
+ 
     }
 }
 // Delete-Modal Box
 function dltModal() {
     let dltModal = document.getElementById("dlt-modal");
     dltModal.style.display = "block";
-    let noBtn = document.getElementById("noBtn");
+    let noBtn = document.getElementById("no-btn");
     noBtn.addEventListener("click", () => {
         dltModal.style.display = "none";
     })
 }
-
-
-
-
+ 
+function edtModal() {
+    let edtModal = document.getElementById("edit-modal");
+    edtModal.style.display = "block";
+    let cancelBtn = document.getElementById("cancel-btn");
+    cancelBtn.addEventListener("click", () => {
+        edtModal.style.display = "none";
+    })
+ 
+}
+ 
+ 
 //filter dropdown
-
-let filter_dropdown = document.getElementById("filter-box-head");
-let filter_list = document.getElementById("filter-list");
-filter_dropdown.addEventListener("click", () => {
-
-    if (filter_list.style.display == "block") {
-        filter_list.style.display = "none";
+ 
+let filterDropdown = document.getElementById("filter-box-head");
+let filterList = document.getElementById("filter-list");
+filterDropdown.addEventListener("click", () => {
+ 
+    if (filterList.style.display == "block") {
+        filterList.style.display = "none";
     } else
-        filter_list.style.display = "block";
+        filterList.style.display = "block";
 })
-
-
+ 
+ 
 //skill dropdown inside Add-Modal box
-
+ 
 fetch("./skills.json")
     .then(resp => resp.json())
     .then(skills => skillList(skills));
-
+ 
 function skillList(skills) {
     skills.forEach(item => {
-        let skill_list = document.getElementById("skill-list");
-        let list_item = document.createElement("li");
-        list_item.setAttribute("class", "skill-list-items")
-        list_item.innerHTML = `<li>${item.skill_name}</li>`;
-        skill_list.appendChild(list_item);
+        let skillList = document.getElementById("skill-list");
+        let listItem = document.createElement("li");
+        listItem.setAttribute("class", "skill-list-items")
+        listItem.innerHTML = `<li>${item.skill_name}</li>`;
+        skillList.appendChild(listItem);
     });
     let skill_dropdown = document.getElementById("skill-box-head");
-    let skill_list = document.getElementById("skill-list");
+    let skillList = document.getElementById("skill-list");
     skill_dropdown.addEventListener("click", () => {
-
-        if (skill_list.style.display == "block") {
-            skill_list.style.display = "none";
+ 
+        if (skillList.style.display == "block") {
+            skillList.style.display = "none";
         } else
-            skill_list.style.display = "block";
+            skillList.style.display = "block";
     })
 }
 //sort dropdown
-
-let sort_dropdown = document.getElementById("sort-box-head");
-let sort_list = document.getElementById("sort-list");
-sort_dropdown.addEventListener("click", () => {
-
-    if (sort_list.style.display == "block") {
-        sort_list.style.display = "none";
+ 
+let sortDropdown = document.getElementById("sort-box-head");
+let sortList = document.getElementById("sort-list");
+sortDropdown.addEventListener("click", () => {
+ 
+    if (sortList.style.display == "block") {
+        sortList.style.display = "none";
     } else
-        sort_list.style.display = "block";
+        sortList.style.display = "block";
 })
